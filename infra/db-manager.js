82390377c08176e8179892fa2aecf0aec0ec5164
Vítor Marçal <git@vitormarcal.com.br>
2023-04-27 14:08:08 -0300

    fecha conexão

--- infra/db-manager.js
+++ infra/db-manager.js
@@ -3,8 +3,8 @@ const db = new sqlite3.Database('./whatsapp_arquiver.sqlite');
 const fs = require('fs');
 const path = require("path");
 function createTables() {
-    const creaateTableFile = path.join(__dirname, './migrations/create_table.sql')
-    fs.readFile(creaateTableFile, 'utf8', (err, sql) => {
+    const createTableScriptPath = path.join(__dirname, './migrations/create_table.sql')
+    fs.readFile(createTableScriptPath, 'utf8', (err, sql) => {
         if (err) {
             throw err;
         }
@@ -14,6 +14,7 @@ function createTables() {
                     throw err;
                 }
                 console.log('Tabelas criadas com sucesso!');
+                db.close();
             });
         });
     });
