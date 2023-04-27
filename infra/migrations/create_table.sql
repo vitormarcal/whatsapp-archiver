98dd974ab24dee51b17d16127cc5f7cb465f078a
Vítor Marçal <git@vitormarcal.com.br>
2023-04-27 14:06:23 -0300

    adiciona script de criação de tabelas

---
+++ infra/migrations/create_table.sql
@@ -0,0 +1,23 @@
+CREATE TABLE IF NOT EXISTS chat (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS attachment (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL,
+    path TEXT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS message_item (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    author TEXT NULL,
+    message_content TEXT NULL,
+    chat_id INTEGER NOT NULL,
+    attachment_id INTEGER NULL,
+    date TEXT NOT NULL,
+
+    CONSTRAINT FOREIGN KEY (chat_id) REFERENCES chat (id)
+    CONSTRAINT FOREIGN KEY (attachment_id) REFERENCES attachment (id)
+);
+
