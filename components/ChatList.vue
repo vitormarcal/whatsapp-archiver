7a800781ffe5bc9bba21226a8b9b202eb9c1a1cd
Vítor Marçal <codehero@marcal.dev>
2023-05-05 08:27:33 -0300

    create chat list and chat area (#8)
    Reviewed-on: http://192.168.1.17:8418/vitor/whatsapp-archiver/pulls/8

---
+++ components/ChatList.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="chat-list">
+        <template v-for="item in chats.data">
+            <ChatItem :item="item"/>
+        </template>
+
+    </div>
+</template>
+<script>
+
+export default {
+    props: ['chats'],
+    created() {
+    }
+}
+
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,19 @@
 <template>
-  <Tutorial/>
+
+  <div class="container-fluid">
+      <ChatList :chats="chats"/>
+
+      <MessageArea />
+  </div>
+
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+    async asyncData({ $axios }) {
+      const chats = await $axios.$get('http://localhost:3000/api/chats/')
+      return { chats }
+  }
 }
 </script>
