346c1f88258f61ce68cb51dd6a0bbbe67c7ebbfd
Vítor Marçal <git@vitormarcal.com.br>
2023-05-13 12:15:47 -0300

    update port

--- backend/index.js
+++ backend/index.js
@@ -31,7 +31,7 @@ module.exports = app
 
 // Start standalone server if directly running
 if (require.main === module) {
-    const port = process.env.PORT || 3001
+    const port = process.env.PORT || 3007
     app.listen(port, () => {
         // eslint-disable-next-line no-console
         console.log(`API server listening on port ${port}`)
--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'server',
 
+  server: {
+    port: 3007
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'whatsapp-archiver',
