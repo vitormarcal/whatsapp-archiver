0b448ded2f24034c4adfef795215c6a8da008283
Vítor Marçal <codehero@marcal.dev>
2023-05-13 19:42:15 -0300

    adiciona env e salva na tabela parametro

--- backend/models/index.js
+++ backend/models/index.js
@@ -15,6 +15,21 @@ sequelize.sync()
     .then(() => console.log('Modelos sincronizados com sucesso!'))
     .catch(err => console.log('Erro ao sincronizar modelos:', err));
 
+
+const path = require("path");
+Parameter.findOrCreate({
+    where: {name: 'SRC_DIR'},
+    defaults: {
+        value: process.env.SRC_DIR || path.join(__dirname, '../whatsapp/source')
+    }
+}).then(([parameter]) => console.log(`SRC_DIR=${parameter.value}`))
+Parameter.findOrCreate({
+    where: {name: 'ARCHIVE_DIR'},
+    defaults: {
+        value: process.env.ARCHIVE_DIR || path.join(__dirname, '../whatsapp/archive')
+    }
+}).then(([parameter]) => console.log(`ARCHIVE_DIR=${parameter.value}`))
+
 module.exports = {
     Chat,
     Message,
