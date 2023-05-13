f0260f13f8b35d7f60ebd2c9588c77c0aea751bb
Vítor Marçal <git@vitormarcal.com.br>
2023-05-13 11:41:39 -0300

    adiciona path do database  no env

--- backend/models/sequelize.js
+++ backend/models/sequelize.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 const path = require("path");
-const databasePath = path.join(__dirname, '../whatsapp_arquiver.sqlite')
+const databasePath = process.env.WHATSAPP_ARCHIVER_DATABASE_PATH || path.join(__dirname, '../whatsapp_arquiver.sqlite')
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
