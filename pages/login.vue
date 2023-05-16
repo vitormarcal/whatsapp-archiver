c6f0138ddf36d9a92d1d38fdffec71dfeaebbdee
Vítor Marçal <codehero@marcal.dev>
2023-05-16 08:16:30 -0300

    habilita store e cria pagina de login sem ação

---
+++ pages/login.vue
@@ -0,0 +1,41 @@
+<template>
+    <main class="container-fluid">
+        <form @submit.prevent="userLogin">
+            <div>
+                <label>Email</label>
+                <input type="text" v-model="login.email" />
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="text" v-model="login.password" />
+            </div>
+            <div>
+                <button type="submit">Submit</button>
+            </div>
+        </form>
+    </main>
+</template>
+
+<script>
+export default {
+    name: "LoginPage",
+    data() {
+        return {
+            login: {
+                email: '',
+                password: ''
+            }
+        }
+    },
+    methods: {
+        async userLogin() {
+           /* try {
+                let response = await this.$auth.loginWith('local', { data: this.login })
+                console.log(response)
+            } catch (err) {
+                console.log(err)
+            }*/
+        }
+    }
+}
+</script>
