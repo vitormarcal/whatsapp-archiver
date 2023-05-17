257d3d0f4780bc37aa07b8fdf42c45519739c1e0
Vítor Marçal <codehero@marcal.dev>
2023-05-17 11:50:28 -0300

    estiliza a página inicial

--- pages/login.vue
+++ pages/login.vue
@@ -1,18 +1,40 @@
 <template>
     <main class="container-fluid">
-        <form @submit.prevent="userLogin">
+        <h2 class="h2 text-center mt-5">Input your credentials</h2>
+        <b-form @submit.prevent="userLogin" class="d-flex flex-column col-4 ml-auto m-auto">
+
+            <b-form-group
+                id="input-group-email"
+                label="Email address:"
+                label-for="input-email"
+                description="We'll never share your email with anyone else."
+            >
+                <b-form-input
+                    id="input-email"
+                    v-model="login.email"
+                    type="email"
+                    placeholder="Enter email"
+                    required
+                ></b-form-input>
+            </b-form-group>
+
+            <b-form-group
+                id="input-group-password"
+                label="Password:"
+                label-for="input-password"
+            >
+                <b-form-input
+                    id="input-password"
+                    v-model="login.password"
+                    type="password"
+                    placeholder="Enter Password"
+                    required
+                ></b-form-input>
+            </b-form-group>
             <div>
-                <label>Email</label>
-                <input type="text" v-model="login.email" />
+                <b-button type="submit" variant="success">Login</b-button>
             </div>
-            <div>
-                <label>Password</label>
-                <input type="text" v-model="login.password" />
-            </div>
-            <div>
-                <button type="submit">Submit</button>
-            </div>
-        </form>
+        </b-form>
     </main>
 </template>
 
@@ -30,12 +52,22 @@ export default {
     methods: {
         async userLogin() {
             try {
-                let response = await this.$auth.loginWith('local', { data: this.login })
-                console.log(response)
+                await this.$auth.loginWith('local', { data: this.login })
+                if (this.$auth.loggedIn) {
+                    await this.$router.push('/');
+                } else {
+                    console.log('Usuário não está logado.');
+                }
             } catch (err) {
                 console.log(err)
             }
         }
+    },
+    beforeCreate() {
+        if (this.$auth.loggedIn) {
+            this.$router.push('/');
+        }
+
     }
 }
 </script>
