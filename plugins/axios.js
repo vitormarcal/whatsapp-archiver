979279e42f13829fec0f53626bc8a463cb5548a3
Vítor Marçal <git@vitormarcal.com.br>
2023-06-21 20:39:11 -0300

    resetOnError: true,

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,6 +2,7 @@ export default function({ app, $axios }) {
     $axios.onError((error) => {
         if (error.response && error.response.status === 401) {
             app.$auth.reset();
+            console.log(app.$auth)
         }
 
         return Promise.reject(error);
